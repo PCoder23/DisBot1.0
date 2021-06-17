a45a89cb6657052bdae76b47be7c8b4e79dbde8a
Pranav K <85278795+PCoder23@users.noreply.github.com>
2021-06-17 10:49:02 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,13 +4,11 @@ import os
 #import dnspython
 import server
 from discord.ext import commands
+from dotenv import load_dotenv
 
+load_dotenv()
 bot = commands.Bot(command_prefix="$")
 TOKEN = os.getenv("DISCORD_TOKEN")
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
